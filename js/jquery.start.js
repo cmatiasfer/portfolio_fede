e22c6cd65a61736431c09e9b8002d5d07625b7e6
mati-remo <32935356+mati-remo@users.noreply.github.com>
2020-09-20 20:19:11 -0300

    add favicon + fix btn menu

--- js/jquery.start.js
+++ js/jquery.start.js
@@ -1,24 +1,42 @@
+var timeAbout;
+
+
 $(document).ready(function () {
 
+  var animation = true;
   $(document).on('click', '#btn-menu', function () {
-    $(this).toggleClass('close');
-    $('#about > div').toggleClass('show-f');
-
     const about = $('#about');
-    if (about.hasClass('show')) {
-      about.css('opacity', 1);
-      about.css('display', 'initial');
-      setTimeout(() => {
-        about.removeClass('show');
-        about.css('opacity', 0);
-      }, 500);
-      setTimeout(() => {
-        about.removeAttr('style');
-      }, 1000);
-    } else {
-      about.toggleClass('show');
+
+    if (animation) {
+      $(this).toggleClass('close');
+      $('#about > div').toggleClass('show-f');
     }
 
+    if (animation) {
+      if (about.hasClass('show')) {
+        animation = false;
+        about.css('opacity', 1);
+        about.css('display', 'initial');
+        setTimeout(() => {
+          console.log("fire opacity")
+          about.css('opacity', 0);
+        }, 800);
+        setTimeout(() => {
+          about.removeAttr('style');
+          about.removeClass('show');
+          animation = true;
+        }, 1300);
+      } else {
+        about.toggleClass('show');
+        setTimeout(function () {
+          if (about.height() <= $(window).height()) {
+            about.css('height', '100vh');
+          } else {
+            about.css('height', 'auto');
+          }
+        }, 20);
+      }
+    }
   });
 
   var time;
@@ -48,7 +66,7 @@ $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     animateIn: 'fadeIn',
     animateOut: 'fadeOut',
-    autoplay: true,
+    autoplay: false,
     autoplayHoverPause: true,
     dots: false,
     items: 1,
